ec5cb168290714b52d200bd3555fd129ca9ea9d0
Sandman <50317879+Sandman90@users.noreply.github.com>
2025-08-11 10:36:09 +0200

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -5,8 +5,8 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
   die;
 }
 
-$option_name = 'hmc_options';
+$option_name = 'hmcoffees';
 delete_option( $option_name );
 // For site options in Multisite.
 delete_site_option( $option_name );
-delete_metadata( 'post', 0, 'hmc_meta', false, true );
+delete_metadata( 'post', 0, 'hmcoffees_meta', false, true );
