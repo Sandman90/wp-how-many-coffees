1c42de8735fdc7e7c421f2f9b7357e73006ce020
Sandman <50317879+Sandman90@users.noreply.github.com>
2025-08-11 10:34:59 +0200

    Update hmc.php

--- hmc.php
+++ hmc.php
@@ -5,7 +5,7 @@
  * Description: How many coffees spent to finish a certain task? Now you can quantify them thanks to the widget associated with each post. You can also use this widget to quantify other quantities such as sugar, for example, or the amount of spoons of a certain ingredient needed.
  * Version: 1.0.0
  * Author: Andrea Epifano (aka Sandman)
- * Author URI: https://www.sandbay.it/home/
+ * Author URI: https://www.sandbay.it/
  * License: GPLv2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  **/
@@ -14,52 +14,52 @@ defined( 'ABSPATH' ) or die( 'Unauthorized Access!' );
 
 
 
-if ( !defined('HMC_PLUGIN_DIR') ) {
+if ( !defined('HMCOFFEES_PLUGIN_DIR') ) {
 
   //DEFINE SOME USEFUL CONSTANTS
-  define('HMC_PLUGIN_VER', '1.0.0');
-  define('HMC_PLUGIN_DIR', plugin_dir_path(__FILE__));
-  define('HMC_PLUGINS_URL', plugins_url('', __FILE__));
-  define('HMC_PLUGINS_BASENAME', plugin_basename(__FILE__));
-  define('HMC_PLUGIN_FILE', __FILE__);
-  define('HMC_PLUGIN_PACKAGE', 'Free'); //DONT CHANGE THIS - BREAKS AUTO UPDATER
+  define('HMCOFFEES_PLUGIN_VER', '1.0.0');
+  define('HMCOFFEES_PLUGIN_DIR', plugin_dir_path(__FILE__));
+  define('HMCOFFEES_PLUGINS_URL', plugins_url('', __FILE__));
+  define('HMCOFFEES_PLUGINS_BASENAME', plugin_basename(__FILE__));
+  define('HMCOFFEES_PLUGIN_FILE', __FILE__);
+  define('HMCOFFEES_PLUGIN_PACKAGE', 'Free'); //DONT CHANGE THIS - BREAKS AUTO UPDATER
 
-  require_once(HMC_PLUGIN_DIR . 'includes/icons.php');
+  require_once(HMCOFFEES_PLUGIN_DIR . 'includes/icons.php');
 
-  if (!function_exists('hmc_add_to_single_content')) {
-    function hmc_add_to_single_content($the_content)
+  if (!function_exists('hmcoffees_add_to_single_content')) {
+    function hmcoffees_add_to_single_content($the_content)
     {
-      $hmc_options = get_option('hmc_options');
-      $hmc_meta = get_post_meta(get_the_ID(), 'hmc_meta', true);
-      if (is_single() && in_the_loop() && !empty($hmc_options) && !empty($hmc_meta)) {
+      $hmcoffees_options = get_option('hmcoffees_options');
+      $hmcoffees_meta = get_post_meta(get_the_ID(), 'hmcoffees_meta', true);
+      if (is_single() && in_the_loop() && !empty($hmcoffees_options) && !empty($hmcoffees_meta)) {
         $total_difficulty = '';
-        $difficulty_icon = (!empty($hmc_options['hmc_custom_icon']) && strlen($hmc_options['hmc_custom_icon']) > 1)
-            ? '<i class="' . $hmc_options['hmc_custom_icon'] . '"></i>'
-            : hmc_get_icons(isset($hmc_options['hmc_icon']) ? $hmc_options['hmc_icon'] : 'coffee');
-        $label = isset($hmc_options['hmc_label']) ? $hmc_options['hmc_label'] . ' ' : '';
-        $classes = isset($hmc_options['hmc_custom_classes']) ? $hmc_options['hmc_custom_classes'] . ' ' : '';
-        $quantity = $hmc_meta['difficulty'] ? $hmc_meta['difficulty'] : 0;
+        $difficulty_icon = (!empty($hmcoffees_options['hmcoffees_custom_icon']) && strlen($hmcoffees_options['hmcoffees_custom_icon']) > 1)
+            ? '<i class="' . $hmcoffees_options['hmcoffees_custom_icon'] . '"></i>'
+            : hmcoffees_get_icons(isset($hmcoffees_options['hmcoffees_icon']) ? $hmcoffees_options['hmcoffees_icon'] : 'coffee');
+        $label = isset($hmcoffees_options['hmcoffees_label']) ? $hmcoffees_options['hmcoffees_label'] . ' ' : '';
+        $classes = isset($hmcoffees_options['hmcoffees_custom_classes']) ? $hmcoffees_options['hmcoffees_custom_classes'] . ' ' : '';
+        $quantity = $hmcoffees_meta['difficulty'] ? $hmcoffees_meta['difficulty'] : 0;
         if ($quantity > 0) {
           for ($x = 0; $x < $quantity; $x++) {
             $total_difficulty .= $difficulty_icon;
           }
-          $hmc_content =  '<div id="hmc-content" class="' . $hmc_options['hmc_position'] . ' ' . $classes . '" title="' . $hmc_options['hmc_tooltip'] . '">';
-          $hmc_content .=   (!empty($label) ? '<span>' . $label . '</span>' : '')
-                            . $total_difficulty . hmc_get_fontawesome_copy_license();
-          $hmc_content .= '</div>';
-          $the_content = $hmc_options['hmc_position'] === 'top' ? ($hmc_content . $the_content) : ($the_content . $hmc_content);
+          $hmcoffees_content =  '<div id="hmc-content" class="' . $hmcoffees_options['hmcoffees_position'] . ' ' . $classes . '" title="' . $hmcoffees_options['hmcoffees_tooltip'] . '">';
+          $hmcoffees_content .=   (!empty($label) ? '<span>' . $label . '</span>' : '')
+                            . $total_difficulty . hmcoffees_get_fontawesome_copy_license();
+          $hmcoffees_content .= '</div>';
+          $the_content = $hmcoffees_options['hmcoffees_position'] === 'top' ? ($hmcoffees_content . $the_content) : ($the_content . $hmcoffees_content);
         }
       }
       return $the_content;
     }
   }
-  add_filter('the_content', 'hmc_add_to_single_content', 20);
+  add_filter('the_content', 'hmcoffees_add_to_single_content', 20);
 
-  function hmc_load_plugin_css() {
+  function hmcoffees_load_plugin_css() {
     $plugin_url = plugin_dir_url(__FILE__);
     wp_enqueue_style('all', $plugin_url . 'css/all.css');
   }
-  add_action('wp_enqueue_scripts', 'hmc_load_plugin_css');
+  add_action('wp_enqueue_scripts', 'hmcoffees_load_plugin_css');
 
-  require_once(HMC_PLUGIN_DIR . 'includes/admin.php');
+  require_once(HMCOFFEES_PLUGIN_DIR . 'includes/admin.php');
 }
